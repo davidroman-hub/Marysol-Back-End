4e61eec385679e1effde180b85851dceeda5b2bd
davidroman-hub <jobroman83@gmail.com>
2020-03-24 19:21:17 +0100

    adding the characteristic of the all filds have to be required in the product except phot

--- controllers/product.js
+++ controllers/product.js
@@ -16,9 +16,25 @@ exports.create = (req,res) => {
                     error:'La imagen no pudo ser cargada'
                 })
             }
+
+            //All the filds need to be required..
+
+            const {name, description, price, category,shipping, quantity} = fields
+            if(!name || !description || !price || !category || !shipping || !quantity){
+                return res.status(400).json({
+                    error:'Todos los campos son necesarios..'
+                })
+            }
+
                 let product = new Product(fields)
 
                 if(files.photo){
+                //we need to only accept photos with less than 1mb.
+                    if(files.photo.size > 1000000){
+                       return res.status(400).json({
+                           error:'La imagen debe ser menos de 1 mb'
+                       }) 
+                    }
                     product.photo.data = fs.readFileSync(files.photo.path)
                     product.photo.contentType = files.photo.type
                 }
