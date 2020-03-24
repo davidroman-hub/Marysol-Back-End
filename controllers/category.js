fa41de9a81c25bf128f0f9b11e21da71ee5949f7
davidroman-hub <jobroman83@gmail.com>
2020-03-24 22:21:21 +0100

    adding category by id and read the same

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,28 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler')
 
+
+//// find category by id ////
+
+exports.categoryById = (req,res,next,id) => {
+    Category.findById(id).exec((err,category)=>{
+        if(err||!category){
+            return res.status(400).json({
+                error:" La categoria no existe"
+            })
+        }
+        req.category = category
+        next()
+    })
+}
+
+//// read method of category ////
+exports.read = (req,res) => {
+    return res.json(req.category)
+}
+
+
+//// create category method //// 
 exports.create = (req,res) => {
     const category = new Category(req.body)
     category.save((err, data) => {
--- routes/category.js
+++ routes/category.js
@@ -4,12 +4,12 @@ const router = express.Router()
 // to the admin can only create category
         //isadmin           //isauth
 const {adminMiddleware,requireSignin } = require('../controllers/auth') 
-//const {read} = require('../controllers/user')
 
-
-const {create} = require('../controllers/category')
+const {categoryById,read,create} = require('../controllers/category')
 
  router.post('/category/create/:Id',requireSignin,adminMiddleware,create)
+ router.get('/category/:categoryId', read)
 
 
+ router.param("categoryId",categoryById)
  module.exports = router
